551f6eba469aee86b3b821513052f226e5d07771
Miles <41923432+MilesBradt@users.noreply.github.com>
2020-09-14 14:57:42 -0700

    Clean up code and add support for animations

--- ThirdPersonMovement.cs
+++ ThirdPersonMovement.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 // Most of this code is from Brackeys on YouTube
 public class ThirdPersonMovement : MonoBehaviour
-   
+
 {
+    Animator anim;
+    Vector3 velocity;
     public CharacterController controller;
     public Transform cam;
-
+   
     public float speed;
     public float turnSmoothTime = 0.1f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
-    public float walkingSpeed = 6f;
+    public float walkingSpeed = 3f;
     public float runningSpeed = 12f;
     public float groundDistance = 0.4f;
 
@@ -22,36 +24,52 @@ public class ThirdPersonMovement : MonoBehaviour
 
     float turnSmoothVelocity;
 
-    Vector3 velocity;
     bool isGrounded;
     public bool isRunning = false;
 
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        Movement();
+    }
 
-        if (isGrounded && velocity.y < 0) {
+    void Movement()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+       
+        if (isGrounded && velocity.y < 0)
+        {
             velocity.y = -2f;
         }
 
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        
+
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
+        if (direction.magnitude < 0.1f)
+        {
+            MovementTypeForAnimations("idle");
+        }
+
         if (direction.magnitude >= 0.1f)
-        {   
-            // Modified code
+        {
+            
             float verticalConvert = Mathf.Abs(vertical);
             float horizontalConvert = Mathf.Abs(horizontal);
 
             if (isRunning == false && isGrounded)
             {
+                MovementTypeForAnimations("walking");
                 speed = ((verticalConvert + horizontalConvert) * walkingSpeed);
-                // Stops max speed from going above the highest speed value if vertical or horizontal is fully pushed
+                
                 if (speed >= walkingSpeed)
                 {
                     speed = walkingSpeed;
@@ -60,18 +78,15 @@ public class ThirdPersonMovement : MonoBehaviour
 
             if (Input.GetButtonDown("Run") && isGrounded)
             {
+                MovementTypeForAnimations("running");
                 isRunning = true;
                 speed = runningSpeed;
-
-                Debug.Log("running: " + isRunning);
             }
             else if (Input.GetButtonUp("Run"))
             {
                 isRunning = false;
-                Debug.Log("running: " + isRunning);
             }
 
-            // Brackeys' code continued
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
@@ -83,7 +98,29 @@ public class ThirdPersonMovement : MonoBehaviour
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
+    }
 
+    void MovementTypeForAnimations(string type)
+    {
+
+        if (type == "idle")
+        {
+            anim.SetBool("Idle", true);
+            anim.SetBool("Walking", false);
+            anim.SetBool("Running", false);
+        }
+        if (type == "walking")
+        {
+            anim.SetBool("Idle", false);
+            anim.SetBool("Walking", true);
+            anim.SetBool("Running", false);
+        }
+        if (type == "running")
+        {
+            anim.SetBool("Idle", false);
+            anim.SetBool("Walking", false);
+            anim.SetBool("Running", true);
+        }
     }
 
 }
